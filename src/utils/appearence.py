88093ba35e44c0815c41d322d74c552231212fce
Giacomo <giaco5988@gmail.com>
2021-01-01 20:05:51 +0100

    display nodes with multiple marriages

--- src/utils/appearence.py
+++ src/utils/appearence.py
@@ -2,7 +2,7 @@
 It defines family tree appearance
 """
 from abc import abstractmethod
-from typing import Tuple
+from typing import Tuple, Sequence, List
 
 
 class AbstractAppearance:
@@ -18,6 +18,11 @@ class AbstractAppearance:
     @staticmethod
     @abstractmethod
     def couple(male_name: str, female_name: str) -> str:
+        return ""
+
+    @staticmethod
+    @abstractmethod
+    def multi_couple(couples: List[Sequence[str]]) -> str:
         return ""
 
     @staticmethod
@@ -40,3 +45,11 @@ class SimpleAppearance(AbstractAppearance):
     def edge_str(node_name: str, parents_node: str, is_male: bool, num_spouse: int) -> Tuple[str, str]:
         place = 'f0' if is_male or num_spouse == 0 else 'f2'
         return f'{parents_node}:f1', f'{node_name}:{place}'
+
+    @staticmethod
+    def multi_couple(couples: List[Sequence[str]]) -> str:
+        ans = ""
+        for ind, (male, female) in enumerate(couples):
+            ind_1, ind_2, ind_3 = 3*ind, 3*ind+1, 3*ind+2
+            ans = ans + "|" + "{" + f'<f{ind_1}> {male} |<f{ind_2}>|<f{ind_3}> {female}' + "}"
+        return "{" + ans[1:] + "}"
