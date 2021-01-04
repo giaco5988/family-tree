dca9ea8d2987f8ddb1f9020971adaeaab036d715
Giacomo <giaco5988@gmail.com>
2021-01-04 18:48:11 +0100

    use canossa family as default

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from utils.appearence import SimpleAppearance
 from utils.graph import GraphAssembler
 from pathlib import Path
 from utils.utils import update_file_name
+from config import path_config
 
 LOGGER = logging.getLogger(__name__)
 
@@ -26,7 +27,8 @@ def cli():
 def main():
     args = cli()
     if not args.data_path:
-        args.data_path = input("Please enter input csv table path:")
+        tmp = input("Please enter input csv table path (press enter to use example of Canossa family):")
+        args.data_path = tmp if len(tmp) > 0 else os.path.join(path_config.DOCS_PATH, 'canossa_family.csv')
     out_file = update_file_name(os.path.join(args.out_dir, 'family_tree.gv'))
     LOGGER.info(f'Load family connection from  {args.data_path}')
     LOGGER.info(f'Save family tree to {out_file}')
